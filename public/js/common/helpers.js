474cc5c10c9a07aa73614173d4fecd41d97941fb
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-01-06 08:40:16 +0200

    Small fixes

--- public/js/common/helpers.js
+++ public/js/common/helpers.js
@@ -48,9 +48,9 @@ function dateTimeToString(date) {
 		((date.getMonth() + 1) + '').padStart(2, '0'), '-',
 		(date.getDate() + '').padStart(2, '0'),
 		' ',
-		(date.getHours() + '').padStart(2, '0'), ':',
-		(date.getMinutes() + '').padStart(2, '0'), ':',
-		(date.getSeconds() + '').padStart(2, '0'),
+		(date.getHours() + '').padStart(2, '0'),
+		':', (date.getMinutes() + '').padStart(2, '0'),
+		// ':', (date.getSeconds() + '').padStart(2, '0'),
 	].join('')
 }
 
